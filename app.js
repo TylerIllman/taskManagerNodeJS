633467ea59db6d2fb981082bd9159e4b80cc5190
TylerIllman <tyler.illman.productions@gmail.com>
2023-09-12 12:29:08 +0800

    removed console log statements from app.js

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
-//ttest
-
-console.log('Task Manager App')
 const express = require('express')
 const app = express()
 const tasks = require('./routes/tasks')
 const connectDB = require('./db/connect')
 require('dotenv').config()
 const port = 3000
-
 
 app.use(express.static('./public'))
 
